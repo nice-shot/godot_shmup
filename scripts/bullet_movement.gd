15c66a08eda781a5ebe90bde168842f55cc6cbbd
nice-shot <shefferu@gmail.com>
2020-02-28 21:10:06 +0200

    added score lowering when bullet exits the screen

--- scripts/bullet_movement.gd
+++ scripts/bullet_movement.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
 export var speed = 50
+onready var manager = get_tree().root.get_node("Main") as GameManager
+var destroyed = false
 
 func _physics_process(delta):
 	position.y -= speed * delta
@@ -10,8 +12,10 @@ func _on_Bullet_area_entered(area: Area2D):
 	var asteroid = area.get_parent() as AsteroidMovement
 	if (asteroid):
 		asteroid.explode()
+	destroyed = true
 	queue_free()
 
 
 func _on_VisibilityNotifier2D_screen_exited():
+	if not destroyed: manager.update_score(-5)
 	queue_free()
