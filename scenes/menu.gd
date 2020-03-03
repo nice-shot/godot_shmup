787e04bebe5eeb10481c070bfd728ddfda829548
nice-shot <shefferu@gmail.com>
2020-03-03 23:48:41 +0200

    Added main menu scene

---
+++ scenes/menu.gd
@@ -0,0 +1,23 @@
+extends Node
+
+var _selected_index = 0
+onready var _selections = [$PlaySelectionArrows, $LeaderboardSelectionArrows]
+
+func _ready():
+	var selection : Node2D
+	for selection in _selections:
+		selection.hide()
+	
+	_selections[_selected_index].show()
+
+func _input(event):
+	var new_index:int = _selected_index
+	if event.is_action_pressed("ui_down"):
+		new_index += 1
+	elif event.is_action_pressed("ui_up"):
+		new_index -= 1
+	
+	if _selected_index != new_index:
+		_selections[_selected_index].hide()
+		_selected_index = new_index % len(_selections)
+		_selections[_selected_index].show()
