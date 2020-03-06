7a38cf6e03ec0cb7b03ef69fae35102273ac2415
nice-shot <shefferu@gmail.com>
2020-03-07 01:52:43 +0200

    Added saving of highscore

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -3,6 +3,9 @@ extends Node
 
 var score = 0
 onready var score_text = $ScoreLabel
+var highscore = ["???", 0]
+
+const SAVE_DATA_PATH = "user://local.data"
 
 func update_score(points):
 	score += points
@@ -10,6 +13,7 @@ func update_score(points):
 	
 func _ready():
 	update_score(0)
+	_load_score()
 	$Ship.connect("hit", self, "_on_hit")
 	
 func _on_hit():
@@ -20,9 +24,32 @@ func _on_hit():
 	$DeathMessage.connect("finished", self, "_on_continue")
 
 func _on_continue():
+	if (score > highscore[1]):
+		highscore = [$DeathMessage/EnterName.text, score]
+		_save_score()
+	
 	get_tree().paused = false
 	score = 0
+	update_score(0)
 	$DeathMessage.visible = false
 	$DeathMessage.disconnect("finished", self, "_on_continue")
+
+func _load_score():
+	var save_file = File.new()
+	if not save_file.file_exists(SAVE_DATA_PATH):
+		print("No data file found")
+		return
+	save_file.open(SAVE_DATA_PATH, File.READ)
+	print("Loading file: %s" % save_file.get_path_absolute())
+	highscore = parse_json(save_file.get_line())
+	print("Got high score: " + str(highscore))
+	save_file.close()
+
+func _save_score():
+	var save_file = File.new()
+	save_file.open(SAVE_DATA_PATH, File.WRITE)
+	save_file.store_line(to_json(highscore))
+	save_file.close()
+	print("Saving leaderboard: " + str(highscore))
 	
 	
--- scripts/main.gd
+++ scripts/main.gd
@@ -4,7 +4,10 @@ extends Node
 var main_menu = preload("res://scenes/menu.tscn")
 var game = preload("res://scenes/game.tscn")
 
+const SAVE_DATA_PATH = "user://local.data"
+
 var menu_node : Node
+var leaderboard = []
 
 func _ready():
 	menu_node = main_menu.instance()
@@ -15,4 +18,6 @@ func _on_menu_clicked(var id:int):
 	var game_node = game.instance()
 	menu_node.queue_free()
 	add_child((game_node))
+	
+
 	
