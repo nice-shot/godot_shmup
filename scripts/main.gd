72b463be92c7e1733726d4aaee7b2bd3d32e873a
nice-shot <shefferu@gmail.com>
2020-04-09 19:40:58 +0300

    Added shoot sound and changed some things around...

---
+++ scripts/main.gd
@@ -0,0 +1,18 @@
+extends Node
+
+
+var main_menu = preload("res://scenes/menu.tscn")
+var game = preload("res://scenes/game.tscn")
+
+var menu_node : Node
+
+func _ready():
+    menu_node = main_menu.instance()
+    menu_node.connect("menu_clicked", self, "_on_menu_clicked")
+    add_child(menu_node)
+
+func _on_menu_clicked(var id:int):
+    var game_node = game.instance()
+    menu_node.queue_free()
+    add_child((game_node))
+    
--- scripts/menu.gd
+++ scripts/menu.gd
@@ -6,25 +6,26 @@ onready var _selections = [$PlaySelectionArrows, $LeaderboardSelectionArrows]
 signal menu_clicked
 
 func _ready():
-	var selection : Node2D
-	for selection in _selections:
-		selection.hide()
-	
-	_selections[_selected_index].show()
+    var selection : Node2D
+    for selection in _selections:
+        selection.hide()
+    
+    _selections[_selected_index].show()
 
 func _input(event):
-	var new_index:int = _selected_index
-	if event.is_action_pressed("ui_down"):
-		new_index += 1
-	elif event.is_action_pressed("ui_up"):
-		new_index -= 1
-	
-	if _selected_index != new_index:
-		_selections[_selected_index].hide()
-		_selected_index = new_index % len(_selections)
-		_selections[_selected_index].show()
-	
-	if event.is_action_pressed("ui_accept"):
-		# Emitting signal on defered so the ship won't immediately shoot
-		call_deferred("emit_signal", "menu_clicked", _selected_index)
-		
+    var new_index:int = _selected_index
+    if event.is_action_pressed("ui_down"):
+        new_index += 1
+    elif event.is_action_pressed("ui_up"):
+        new_index -= 1
+    
+    if _selected_index != new_index:
+        _selections[_selected_index].hide()
+        _selected_index = new_index % len(_selections)
+        _selections[_selected_index].show()
+    
+    if event.is_action_pressed("ui_accept"):
+        # Emitting signal on defered so the ship won't immediately shoot
+        call_deferred("emit_signal", "menu_clicked", _selected_index)
+
+        
