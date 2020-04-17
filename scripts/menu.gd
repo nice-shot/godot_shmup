a335907151849034a2280647d1c4ffd9e49d1edb
nice-shot <shefferu@gmail.com>
2020-04-17 16:07:47 +0300

    Added play button functionality to main menu and fixed stretching when scaling window

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,32 +1,14 @@
-extends Node
+extends Control
 
-var _selected_index = 0
-onready var _selections = [$PlaySelectionArrows, $LeaderboardSelectionArrows]
 
-signal menu_clicked
+func _on_play() -> void:
+    get_tree().change_scene("res://scenes/game.tscn")
 
-func _ready():
-    for selection in _selections:
-        (selection as Node2D).hide()
-    
-    _selections[_selected_index].show()
 
-func _input(event):
-    var new_index:int = _selected_index
-    if event.is_action_pressed("ui_down"):
-        new_index += 1
-    elif event.is_action_pressed("ui_up"):
-        new_index -= 1
-    
-    if _selected_index != new_index:
-        _selections[_selected_index].hide()
-        _selected_index = new_index % len(_selections)
-        _selections[_selected_index].show()
-    
-    if event.is_action_pressed("ui_accept"):
-        # Emitting signal on defered so the ship won't immediately shoot
-        call_deferred("_emit_menu_clicked")
+func _on_leaderboard() -> void:
+    pass
 
-func _emit_menu_clicked():
-    emit_signal("menu_clicked", _selected_index)
+
+func _on_options() -> void:
+    pass
         
