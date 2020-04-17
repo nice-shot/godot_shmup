c2871d36ecdeaac173e76775836ba8268f97b0d0
nice-shot <shefferu@gmail.com>
2020-04-17 16:25:28 +0300

    Started adding leaderboards menu to main menu scene

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,14 +1,31 @@
-extends Control
+extends Node
+
+onready var _previous_menu_btn : Control = $Main/PlayBTN
 
 
-func _on_play() -> void:
+func _ready() -> void:
+    set_screen_main()
+
+
+func play() -> void:
     get_tree().change_scene("res://scenes/game.tscn")
 
 
-func _on_leaderboard() -> void:
-    pass
+func set_screen_main() -> void:
+    $Main.show()
+    _previous_menu_btn.grab_focus()
+    $Leaderboard.hide()
 
 
-func _on_options() -> void:
-    pass
-        
+func set_screen_leaderboard() -> void:
+    $Leaderboard.show()
+    $Leaderboard/BackBTN.grab_focus()
+    $Main.hide()
+    _previous_menu_btn = $Main/LeaderboardBTN
+
+
+func set_screen_options() -> void:
+    return
+    $Main.hide()
+    $Leaderboard.hide()
+    _previous_menu_btn = $Main/OptionsBTN
