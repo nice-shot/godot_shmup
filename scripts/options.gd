938dbf8e0462c98a22f771a042ec8e8ef0a17501
nice-shot <shefferu@gmail.com>
2020-04-17 23:38:21 +0300

    Updated options script to set volume slider based on current volume

--- scripts/options.gd
+++ scripts/options.gd
@@ -1,14 +1,35 @@
 extends Node
 
-var _volume_steps = {
-    0: -72,
-    1: -30,
-    2: -20,
-    3: -10,
-    4: -5,
-    5: 0
-}
+var _volume_steps = [-72, -30, -20, -10, -5, 0]
+
+const MASTER_BUS := "Master"
+
+
+func _ready() -> void:
+    var step: int = _get_bus_volume_step(MASTER_BUS)
+    var slider: Slider = $VolumeCTRL/VolumeSlider
+    if step != slider.value:
+        slider.value = step
+
+
+func _get_bus_volume(bus: String) -> float:
+    return AudioServer.get_bus_volume_db(AudioServer.get_bus_index(bus))
+
+
+func _set_bus_volume(bus: String, volume: float) -> void:
+    AudioServer.set_bus_volume_db(AudioServer.get_bus_index(bus), volume)
+
+
+func _get_bus_volume_step(bus: String) -> int:
+    var bus_volume: float = _get_bus_volume(bus)
+    for i in range(_volume_steps.size()):
+        var val: int = _volume_steps[i]
+        if bus_volume <= val:
+            return i
+    # Returns 0 db value
+    return _volume_steps.size() - 1
+
 
 func _on_volume_change(value: int) -> void:
-    AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), _volume_steps[value])
+    _set_bus_volume(MASTER_BUS, _volume_steps[value])
     get_parent().get_node("SFX").play()
