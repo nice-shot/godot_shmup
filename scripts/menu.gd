6f4392f05057700a81f8d13b3d19ac464046d169
nice-shot <shefferu@gmail.com>
2020-04-17 23:37:22 +0300

    Added arrows around volume control and added focus sound

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -6,14 +6,18 @@ onready var _previous_menu_btn : Control = $Main/PlayBTN
 func _ready() -> void:
     set_screen_main()
     # Set SelectSFX for button switching
-    for button in _get_nodes_of_type(Button, self):
+    for button in _get_nodes_of_type([Button, Slider], self):
         button.connect("focus_entered", $SFX, "play")
 
 
 func _get_nodes_of_type(type, root : Node) -> Array:
     var nodes := []
     for n in root.get_children():
-        if n is type:
+        if type is Array:
+            for t in type:
+                if n is t:
+                    nodes.append(n)
+        elif n is type:
             nodes.append(n)
         for sub_n in _get_nodes_of_type(type, n):
             nodes.append(sub_n)
